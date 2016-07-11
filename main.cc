4a2ea5750803c079d1f0cd367380667bcd6b9a14
W.H.Zwart <walther.zwart@gmail.com>
2016-07-11 12:36:00 +0200

    Started text example

--- main.cc
+++ main.cc
@@ -7,34 +7,21 @@ int main()
 {
     using namespace graphics2;
 
-    auto width = 600;
-    auto height = 400;
-    image_surface_t surface(graphics2::Format::FORMAT_ARGB32, width, height);
+    const auto HEIGHT = 200.0;
+    const auto WIDTH = 400.0;
+    const auto FONT_SIZE = 64.0;
+    const auto text_origin = pos_t(50.0, (HEIGHT / 2.0) + (FONT_SIZE / 2.0));
 
-    surface.fill(color_t(0.86, 0.85, 0.47));
+    image_surface_t surface(Cairo::FORMAT_ARGB32, WIDTH, HEIGHT);
+    auto white = color_t(1, 1, 1);
+    surface.fill(white);
+    auto arc = arc_t(TEXT_ORIGIN_X, TEXT_ORIGIN_Y, FONT_SIZE / 4.0, 0, 2*M_PI);
+    auto green = color_t(0, 1, 0, 0.5);
+    surface.fill(arc, green);
 
-    surface.stroke(pen_t(20), rectangle_t(0, 0, width, height));
-
-    auto pen = pen_t(color_t(0, 0, 0, 0.7), 20);
-    surface.stroke(
-        pen,
-        arc_t(
-            width/2,
-            height/2,
-            height/4,
-            0,
-            2*M_PI));
-
-    surface.stroke(
-        pen,
-        line_t(
-            width / 4.0,
-            height / 4.0,
-            width * 3.0 / 4.0,
-            height * 3.0 / 4.0));
-
-    std::string filename = "image.png";
-    surface.write_to_png(filename);
-
-    std::cout << "Wrote png file \"" << filename << "\"" << std::endl;
+    auto red = color_t(0.8, 0.2, 0.2);
+    auto font_face = toy_font_face_t("Bitstream Charter", Cairo::FONT_SLANT_ITALIC, Cairo::FONT_WEIGHT_BOLD);
+    auto font = font_t(font_face, red, FONT_SIZE);
+    surface.print(font, TEXT_ORIGIN_X, TEXT_ORIGIN_Y, "graphics2!");
+    surface.write_to_png("toy-text.png");
 }
